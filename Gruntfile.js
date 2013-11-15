f641af0087928a045a71d80f4dd4f39d2b8bc17a
Stefan Birkner <mail@stefan-birkner.de>
2013-11-15 01:18:35 +0100

    Replace Makefile by Grunt deploy task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,14 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     clean: ['node_modules', 'target', '_site'],
+    exec: {
+      before_package: {
+        command: 'rm -rf target;git clone git@github.com:stefanbirkner/stefanbirkner.github.io.git target;cd target;git rm -rf *'
+      },
+      prepare_deployment: {
+        command: 'cd target; git add .'
+      }
+    },
     jekyll: {
       serve: {
         src: 'src/main/webapp',
@@ -14,7 +22,8 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-jekyll');
   grunt.registerTask('default', ['jekyll:serve']);
-  grunt.registerTask('package', ['jekyll:package']);
+  grunt.registerTask('deploy', ['exec:before_package', 'jekyll:package', 'exec:prepare_deployment']);
 };
