9c9c3bba031785696a20e89d616cccc6d412ae5d
Stefan Birkner <mail@stefan-birkner.de>
2013-11-17 13:51:05 +0100

    Extend deploy task. It now deploy to github.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,22 @@ module.exports = function(grunt) {
   grunt.initConfig({
     clean: ['node_modules', 'target', '_site'],
     exec: {
-      before_package: {
-        command: 'rm -rf target;git clone git@github.com:stefanbirkner/stefanbirkner.github.io.git target;cd target;git rm -rf *'
+      clear_deploy_repo: {
+        command: 'git rm -rf *',
+        cwd: 'target'
       },
-      prepare_deployment: {
-        command: 'cd target; git add .'
+      clone_deploy_repo: {
+        command: 'rm -rf target;git clone git@github.com:stefanbirkner/stefanbirkner.github.io.git target'
+      },
+      deploy_to_github: {
+        command: 'git add .;cat ../revision | xargs -i echo "Revision stefanbirkner/stefan-birkner.de@"{} | xargs -i git commit -m "{}"; git push',
+        cwd: 'target'
+      },
+      delete_revision_file: {
+        command: 'rm revision'
+      },
+      write_revision_file: {
+        command: 'git rev-parse HEAD > revision'
       }
     },
     jekyll: {
@@ -25,5 +36,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-jekyll');
   grunt.registerTask('default', ['jekyll:serve']);
-  grunt.registerTask('deploy', ['exec:before_package', 'jekyll:package', 'exec:prepare_deployment']);
+  grunt.registerTask('deploy', ['exec:clone_deploy_repo', 'exec:clear_deploy_repo', 'jekyll:package', 'exec:write_revision_file', 'exec:deploy_to_github', 'exec:delete_revision_file']);
 };
