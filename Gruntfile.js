69d04f43ac059c3d8785d5397e9c29e70fd5df25
Stefan Birkner <mail@stefan-birkner.de>
2014-05-24 15:39:18 +0200

    Using grunt-build-control for deployment.
    This grunt plugin does the same as my own script. No need to
    do it myself.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,20 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    clean: ['node_modules', 'target', '_site'],
-    exec: {
-      clear_deploy_repo: {
-        command: 'git rm -rf *',
-        cwd: 'target'
+    buildcontrol: {
+      options: {
+        dir: 'target',
+        commit: true,
+        push: true,
+        message: 'Revision stfanbirkner/stefan-birkner.de@%sourceCommit%'
       },
-      clone_deploy_repo: {
-        command: 'rm -rf target;git clone -b gh-pages git@github.com:stefanbirkner/stefan-birkner.de.git target'
-      },
-      deploy_to_github: {
-        command: 'git add .;cat ../revision | xargs -i echo "Revision stefanbirkner/stefan-birkner.de@"{} | xargs -i git commit -m "{}"; git push',
-        cwd: 'target'
-      },
-      delete_revision_file: {
-        command: 'rm revision'
-      },
-      write_revision_file: {
-        command: 'git rev-parse HEAD > revision'
+      pages: {
+        options: {
+          remote: 'git@github.com:stefanbirkner/stefan-birkner.de.git',
+          branch: 'gh-pages'
+        }
       }
     },
+    clean: ['node_modules', 'target', '_site'],
     jekyll: {
       serve: {
         options: {
@@ -36,9 +31,9 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-build-control');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-jekyll');
   grunt.registerTask('default', ['jekyll:serve']);
-  grunt.registerTask('deploy', ['exec:clone_deploy_repo', 'exec:clear_deploy_repo', 'jekyll:package', 'exec:write_revision_file', 'exec:deploy_to_github', 'exec:delete_revision_file']);
+  grunt.registerTask('deploy', ['jekyll:package', 'buildcontrol:pages']);
 };
